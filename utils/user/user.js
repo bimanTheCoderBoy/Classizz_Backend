622fe01a3692c24b21cbcd6ed18d0b370eac047b
Biman Kumar Das <daskumarbiman2020@gmail.com>
2024-01-12 19:41:16 +0530

    user route03

--- utils/user/user.js
+++ utils/user/user.js
@@ -2,16 +2,16 @@ const Teacher =require("../../models/teacher")
 const Student =require("../../models/student")
 const Errorx = require("../error/customError")
 
-module.exports=(req,res,next)=>{
-    const email=req.body.params
+module.exports=async(req,res,next)=>{
+    const email=req.params.email
     if(!email)
     next(new Errorx("not a valid request",404))
     else{
-      const teacher=  Teacher.findOne({email:email})
+      const teacher= await Teacher.findOne({email:email})
       if(teacher){
         res.send(true)
       }else{
-        const student=Student.findOne({email})
+        const student=await Student.findOne({email})
         if(student){
             res.send(true)
         } else{
