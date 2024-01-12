affb03568b5d7fea32ab6b9b2fec4853cf9cd767
Biman Kumar Das <daskumarbiman2020@gmail.com>
2024-01-12 19:52:51 +0530

    user route04

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const setRun=async()=>{
     const  app=express();
     app.use(express.json())
     app.use(cookieParser())   
-     app.use(cors("/*"))
+     app.use(cors("http://localhost:5173"))
     app.use("/user",userRoutes)
     app.use("/student",studentRoutes)
     app.use("/teacher",teacherRoutes)
